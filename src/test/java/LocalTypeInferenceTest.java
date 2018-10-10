4234ad111d06fa3e8286ade77ac6b8ab9f6f0f25
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-10-10 18:36:44 +0200

    LocalTypeInferenceTest.java try-with-resources

--- src/test/java/LocalTypeInferenceTest.java
+++ src/test/java/LocalTypeInferenceTest.java
@@ -3,22 +3,11 @@ import org.junit.Test;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Set;
-
-import static org.hamcrest.Matchers.hasSize;
-import static org.junit.Assert.assertThat;
 
 /**
  * Created by mtumilowicz on 2018-10-10.
  */
 public class LocalTypeInferenceTest {
-    
-    @Test
-    public void basic() {
-        var intSet = Set.of(1, 2, 3);
-        
-        assertThat(intSet, hasSize(3));
-    }
     
     @Test
     public void try_with_resources() throws IOException {
